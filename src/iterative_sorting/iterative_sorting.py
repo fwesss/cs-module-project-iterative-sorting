93339ca14868ae7a7f405c067b28358546c291c9
Westley Feller <feller.westley@gmail.com>
2020-08-03 20:32:24 -0700

    Bubble sort complete

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
@@ -8,7 +11,6 @@ def selection_sort(arr):
         # (hint, can do in 3 loc)
         # Your code here
 
-
         # TO-DO: swap
         # Your code here
 
@@ -16,13 +18,21 @@ def selection_sort(arr):
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort(arr):
-    # Your code here
-
+def bubble_sort(arr: List[int]) -> List[int]:
+    swapped = True
+    while swapped:
+        swapped = False
+        for index in range(len(arr) - 1):
+            if arr[index] > arr[index + 1]:
+                temp = arr[index + 1]
+                arr[index + 1] = arr[index]
+                arr[index] = temp
+                swapped = True
 
     return arr
 
-'''
+
+"""
 STRETCH: implement the Counting Sort function below
 
 Counting sort is a sorting algorithm that works on a set of data where
@@ -38,9 +48,10 @@ showed up, we can construct a sorted set of the input data from the
 buckets. 
 
 What is the time and space complexity of the counting sort algorithm?
-'''
+"""
+
+
 def counting_sort(arr, maximum=None):
     # Your code here
-
 
     return arr
