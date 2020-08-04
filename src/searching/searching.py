326cc8e030df223c624b6827c8d35c0630688417
Westley Feller <feller.westley@gmail.com>
2020-08-04 10:29:10 -0700

    Binary search complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,34 @@
-def linear_search(arr, target):
-    # Your code here
+from typing import List
 
 
-    return -1   # not found
+def linear_search(arr: List[int], target: int) -> int:
+    for index, number in enumerate(arr):
+        if number is target:
+            return index
+
+    return -1  # not found
 
 
 # Write an iterative implementation of Binary Search
-def binary_search(arr, target):
+def binary_search(arr: List[int], target: int) -> int:
+    if len(arr) > 0:
+        left = 0
+        right = len(arr) - 1
 
-    # Your code here
+        while left != right:
+            middle = (right - left) // 2
+            if arr[left] is target:
+                return left
 
+            if arr[middle] is target:
+                return middle
+
+            if arr[right] is target:
+                return right
+
+            if target < arr[middle]:
+                right = middle
+            else:
+                left = middle
 
     return -1  # not found
